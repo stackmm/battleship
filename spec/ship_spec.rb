577cffd1af01f96ad4639f0c52395aca1785ed72
Jimmy Jurado <jurado140@gmail.com>
2023-04-06 17:23:38 -0400

    Add initialize spec for ship object.

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 RSpec.describe Ship do
-  
+  describe '#initialize' do
+    it 'exists' do
+      cruiser = Ship.new('Cruiser', 3)
+
+      expect(cruiser).to be_a(Ship)
+    end
+  end
 end
