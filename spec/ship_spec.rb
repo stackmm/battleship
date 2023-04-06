7cda8054fec691da44d8404ad304838840cd3774
Jimmy Jurado <jurado140@gmail.com>
2023-04-06 17:36:17 -0400

    Adds ship attribute tests.

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,11 +1,19 @@
 require 'spec_helper'
 
 RSpec.describe Ship do
+  brefore(:each) do
+    @cruiser = Ship.new('Cruiser', 3)
+  end
+
   describe '#initialize' do
     it 'exists' do
-      cruiser = Ship.new('Cruiser', 3)
+      expect(@cruiser).to be_a(Ship)
+    end
 
-      expect(cruiser).to be_a(Ship)
+    it 'has attributes' do
+      expect(@cruiser.name).to eq("Cruiser")
+      expect(@cruiser.length).to eq(3)
+      expect(@cruiser.health).to eq(3)
     end
   end
 end
