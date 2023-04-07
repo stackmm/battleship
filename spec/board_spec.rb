598e1ad0b13b0e44570aef63fcc396888779eca5
Jimmy Jurado <jurado140@gmail.com>
2023-04-07 13:27:59 -0400

    Add spec test for board initialized instance.

---
+++ spec/board_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+RSpec.describe Board do
+  before(:each) do
+    @board = Board.new
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(@board).to be_a(Board)
+    end
+    it 'has cells attributes' do
+      expect(@board.cells).to be_a(Hash)
+      expect(@board.cells.count).to eq(16)
+      expect(@board.cells.values).to be_a(Cell)
+    end
+  end
+end
