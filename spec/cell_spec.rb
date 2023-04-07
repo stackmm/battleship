9192a2fc1ff3b0617ab7e236e3efbd7cb8e608c6
Steven Stackhouse <stackmm@gmail.com>
2023-04-06 20:25:17 -0600

    add #fired_upon? tests

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 RSpec.describe Cell do
   before(:each) do
     @cell = Cell.new('B4')
+    @cruiser = Ship.new('Cruiser', 3)
   end
 
   describe '#initialize' do
@@ -19,6 +20,21 @@ RSpec.describe Cell do
   describe '#empty?' do
     it 'can check if a cell is empty' do
       expect(@cell.empty?).to be true
+    end
+  end
+
+  describe '#place_ship' do
+    it 'can place a ship' do
+      @cell.place_ship(@cruiser)
+      expect(@cell.ship).to eq(@cruiser)
+      expect(@cell.empty?).to be false
+    end
+  end
+
+  describe '#fired_upon' do
+    it 'can check if a ship has been fired upon' do
+      @cell.place_ship(@cruiser)
+      expect(@cell.fired_upon?).to be false
     end
   end
 
