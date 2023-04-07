ed3f1cadd0c75d328f2ccbc45abce22a7b3b1422
Steven Stackhouse <stackmm@gmail.com>
2023-04-06 20:12:45 -0600

    add #empty? tests

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -2,17 +2,23 @@ require 'spec_helper'
 
 RSpec.describe Cell do
   before(:each) do
-    @cell = Cell.new("B4")
+    @cell = Cell.new('B4')
   end
 
-  describe "#initialize" do
+  describe '#initialize' do
     it 'exists' do
       expect(@cell).to be_a(Cell)
     end
 
     it 'has attributes' do
-      expect(@cell.coordinate).to eq("B4")
+      expect(@cell.coordinate).to eq('B4')
       expect(@cell.ship).to be nil
+    end
+  end
+
+  describe '#empty?' do
+    it 'can check if a cell is empty' do
+      expect(@cell.empty?).to be true
     end
   end
 
