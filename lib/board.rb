84dae87591707fdf1ac08fc8f131e2c4ef4ff70f
Steven Stackhouse <stackmm@gmail.com>
2023-04-08 13:54:06 -0600

    cleanup syntax

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,6 @@
 class Board
   attr_reader :cells
+  
   def initialize
     @cells = {
       'A1' => Cell.new('A1'),
@@ -25,34 +26,28 @@ class Board
     @cells.keys.include?(coordinate)
   end
 
-  def valid_placement?(ship, ship_coordinates)
-    ## first validates ship length
-    valid_length?(ship, ship_coordinates) && 
-
-    ## if the ship is being placed vertically --> check for valid vertical placement
-    if ship_coordinates.first[1] == ship_coordinates[1][1] && ship_coordinates[1][1] == ship_coordinates.last[1]
-      valid_vertical?(ship, ship_coordinates)
-    else ## otherwise checks for valid horizontal placement if not vertical (diagonals will fail this check)
-      valid_horizontal?(ship, ship_coordinates)
-    end
-  end
-
   def valid_length?(ship, ship_coordinates)
     ship_coordinates.count == ship.length
   end
 
-  def valid_horizontal?(ship, ship_coordinates)
+  def valid_horizontal_placement?(ship, ship_coordinates)
     range = ship_coordinates.first..ship_coordinates.last
     array = range.to_a
     array == ship_coordinates
   end
 
-  # check for appropriate vertical placement by looking at each consecutive pair of coordinates
-  # ie: ["B1", "C1"], then ["C1", "D1"], and assessing if the letters are consecutive ordinal values
-  def valid_vertical?(ship, ship_coordinates)
+  def valid_vertical_placement?(ship, ship_coordinates)
     ship_coordinates.each_cons(2).all? do |ship_coordinate, next_ship_coordinate|
-      # ship_coordinate[1] == next_ship_coordinate[1] &&
       ship_coordinate[0].ord + 1 == next_ship_coordinate[0].ord
+    end
+  end
+
+  def valid_placement?(ship, ship_coordinates)
+    valid_length?(ship, ship_coordinates) && 
+    if ship_coordinates.first[1] == ship_coordinates[1][1] && ship_coordinates[1][1] == ship_coordinates.last[1]
+      valid_vertical_placement?(ship, ship_coordinates)
+    else
+      valid_horizontal_placement?(ship, ship_coordinates)
     end
   end
 
