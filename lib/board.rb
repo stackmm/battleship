2750ae2fea0a385ad44813dc80d9e8c2fdafcb3d
Steven Stackhouse <stackmm@gmail.com>
2023-04-08 13:30:16 -0600

    add complete valid_placement? method with helper methods

--- lib/board.rb
+++ lib/board.rb
@@ -22,26 +22,38 @@ class Board
   end
 
   def valid_coordinate?(coordinate)  
-    # @cells.each do |key, value|
-    #   return true if value.coordinate == coordinate
-    # end
-    # false
     @cells.keys.include?(coordinate)
   end
 
   def valid_placement?(ship, ship_coordinates)
-    range = ship_coordinates.first..ship_coordinates.last
-    array  = range.to_a
+    ## first validates ship length
+    valid_length?(ship, ship_coordinates) && 
 
-    valid_length?(ship, ship_coordinates) && array == ship_coordinates
-
-    # return false unless ship_coordinates.count == ship.length
-    # return false unless array == ship_coordinates
-    
+    ## if the ship is being placed vertically --> check for valid vertical placement
+    if ship_coordinates.first[1] == ship_coordinates[1][1] && ship_coordinates[1][1] == ship_coordinates.last[1]
+      valid_vertical?(ship, ship_coordinates)
+    else ## otherwise checks for valid horizontal placement if not vertical (diagonals will fail this check)
+      valid_horizontal?(ship, ship_coordinates)
+    end
   end
 
   def valid_length?(ship, ship_coordinates)
     ship_coordinates.count == ship.length
+  end
+
+  def valid_horizontal?(ship, ship_coordinates)
+    range = ship_coordinates.first..ship_coordinates.last
+    array = range.to_a
+    array == ship_coordinates
+  end
+
+  # check for appropriate vertical placement by looking at each consecutive pair of coordinates
+  # ie: ["B1", "C1"], then ["C1", "D1"], and assessing if the letters are consecutive ordinal values
+  def valid_vertical?(ship, ship_coordinates)
+    ship_coordinates.each_cons(2).all? do |ship_coordinate, next_ship_coordinate|
+      # ship_coordinate[1] == next_ship_coordinate[1] &&
+      ship_coordinate[0].ord + 1 == next_ship_coordinate[0].ord
+    end
   end
 
 end
