d8b71bb1ba5d42c67c734077845a09ec4982701c
Steven Stackhouse <stackmm@gmail.com>
2023-04-08 19:08:03 -0600

    update #valid_placement? tests for its refactor

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -37,25 +37,25 @@ RSpec.describe Board do
     end
   end
 
-  describe '#valid_horizontal_placement?' do
+  describe '#consecutive_coordinates?' do
     it 'can check for consecutive horizontal coordinates' do
-      expect(@board.valid_horizontal_placement?(@cruiser, ['A1', 'A2', 'A4'])).to be(false)
-      expect(@board.valid_horizontal_placement?(@submarine, ['A1', 'C1'])).to be(false)
-      expect(@board.valid_horizontal_placement?(@cruiser, ['A3', 'A2', 'A1'])).to be(false)
-      expect(@board.valid_horizontal_placement?(@submarine, ['C1', 'B1'])).to be(false)
-      expect(@board.valid_horizontal_placement?(@submarine, ['B1', 'B2'])).to be(true)
-      expect(@board.valid_horizontal_placement?(@cruiser, ['D2', 'D3', 'D4'])).to be(true)
-      expect(@board.valid_horizontal_placement?(@cruiser, ['A1', 'B2', 'C3'])).to be(false)
+      expect(@board.consecutive_coordinates?(@cruiser, ['A1', 'A2', 'A4'])).to be(false)
+      expect(@board.consecutive_coordinates?(@submarine, ['A1', 'C1'])).to be(false)
+      expect(@board.consecutive_coordinates?(@cruiser, ['A3', 'A2', 'A1'])).to be(false)
+      expect(@board.consecutive_coordinates?(@submarine, ['C1', 'B1'])).to be(false)
+      expect(@board.consecutive_coordinates?(@submarine, ['B1', 'B2'])).to be(true)
+      expect(@board.consecutive_coordinates?(@cruiser, ['D2', 'D3', 'D4'])).to be(true)
+      expect(@board.consecutive_coordinates?(@cruiser, ['A1', 'B2', 'C3'])).to be(false)
     end
   end
 
-  describe '#valid_vertical_placement?' do
+  describe '#consecutive_vertical_coordinates?' do
     it 'can check for consecutive vertical coordinates' do
-      expect(@board.valid_vertical_placement?(@submarine, ['A1', 'A2'])).to be(false)
-      expect(@board.valid_vertical_placement?(@cruiser, ['C2', 'C3', 'C4'])).to be(false)
-      expect(@board.valid_vertical_placement?(@cruiser, ['A1', 'B1', 'C1'])).to be(true)
-      expect(@board.valid_vertical_placement?(@submarine, ['C2', 'D2'])).to be(true)
-      expect(@board.valid_vertical_placement?(@cruiser, ['B3', 'C3', 'D3'])).to be(true)
+      expect(@board.consecutive_vertical_coordinates?(@submarine, ['A1', 'A2'])).to be(false)
+      expect(@board.consecutive_vertical_coordinates?(@cruiser, ['C2', 'C3', 'C4'])).to be(false)
+      expect(@board.consecutive_vertical_coordinates?(@cruiser, ['A1', 'B1', 'C1'])).to be(true)
+      expect(@board.consecutive_vertical_coordinates?(@submarine, ['C2', 'D2'])).to be(true)
+      expect(@board.consecutive_vertical_coordinates?(@cruiser, ['B3', 'C3', 'D3'])).to be(true)
     end
   end
 
@@ -63,7 +63,9 @@ RSpec.describe Board do
     it 'it can check for valid ship length and consecutive horizontal/vertical coordinates' do
       expect(@board.valid_placement?(@cruiser, ['A1', 'B2', 'C3'])).to be(false)
       expect(@board.valid_placement?(@submarine, ['C2', 'D3'])).to be(false)
+      expect(@board.valid_placement?(@cruiser, ['D1', 'C2', 'B3'])).to be(false)
       expect(@board.valid_placement?(@submarine, ['A1', 'A2'])).to be(true)
+      expect(@board.valid_placement?(@submarine, ['C3', 'C4'])).to be(true)
       expect(@board.valid_placement?(@cruiser, ['B1', 'C1', 'D1'])).to be(true)
       expect(@board.valid_placement?(@cruiser, ['A1', 'B1', 'C1'])).to be(true)
       expect(@board.valid_placement?(@cruiser, ['B3', 'C3', 'D3'])).to be(true)
