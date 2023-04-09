86a5e75c1a9b21de5c155199064e75c472d39f1e
Jimmy Jurado <jurado140@gmail.com>
2023-04-09 14:05:05 -0400

    Creates the start game method and updates on main menu method.

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,10 @@
 class Game 
+  attr_reader :player, :computer
 
+  def initialize
+    @player = Player.new
+    @computer = Player.new
+  end
 
   def main_menu
     p 'Welcome to BATTLESHIP'
@@ -9,6 +14,9 @@ class Game
         start_game
       elsif response == "q"
         p "Game Ended"
+        main_menu
+      else "Invalid Input"
+        main_menu
       end
   end
 
@@ -16,7 +24,7 @@ class Game
     p 'I have laid out my ships on the grid.'
     p 'You now need to lay out your two ships.'
     p 'The Cruiser is three units long and the Submarine is two units long.'
-    
+    p @player.board.render
   end
 end
 
