b29434654c537c2591f8936bc0f03fef5f082a9b
Steven Stackhouse <stackmm@gmail.com>
2023-04-08 19:57:27 -0600

    add tests for #place

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -72,4 +72,19 @@ RSpec.describe Board do
     end
   end
 
+  describe '#place' do 
+    it 'can place a ship' do 
+      @board.place(@cruiser, ['A1', 'A2', 'A3'])
+      cell_1 = @board.cells['A1']
+      cell_2 = @board.cells['A2']
+      cell_3 = @board.cells['A3']
+      expect(cell_1.ship).to eq(@cruiser)
+      expect(cell_2.ship).to eq(@cruiser)
+      expect(cell_3.ship).to eq(@cruiser)
+      expect(cell_1.ship).to eq(cell_2.ship)
+      expect(cell_2.ship).to eq(cell_3.ship)
+      expect(cell_1.ship).to eq(cell_3.ship)
+    end
+  end
+
 end
