044b50d107f58cfd0579dd1f64b58f5fa1a48387
Jimmy Jurado <jurado140@gmail.com>
2023-04-09 13:44:20 -0400

    Creates the start method. Pending Player and turn functionality.

--- game_runner.rb
+++ game_runner.rb
@@ -3,3 +3,4 @@ require './spec/spec_helper'
 
     game = Game.new
     game.main_menu
+    game.start_game
--- lib/game.rb
+++ lib/game.rb
@@ -5,6 +5,18 @@ class Game
     p 'Welcome to BATTLESHIP'
     p 'Enter p to play. Enter q to quit.'
     response = gets.chomp
+      if response == "p"
+        start_game
+      elsif response == "q"
+        p "Game Ended"
+      end
+  end
+
+  def start_game
+    p 'I have laid out my ships on the grid.'
+    p 'You now need to lay out your two ships.'
+    p 'The Cruiser is three units long and the Submarine is two units long.'
+    
   end
 end
 
